defadd912b17c8888c33259c2f90640d54be5039
Jozsef Marton <marton@db.bme.hu>
2016-03-21 21:57:43 +0100

    Added error handling during deploy. Fix in comments.

--- util/deploy.sh
+++ util/deploy.sh
@@ -12,6 +12,17 @@ WEB_DIRNAME=jdbc
 
 OJDBC7_PATH=/oracle/product/12.1.0/dbhome_1/jdbc/lib/ojdbc7.jar
 
+if [ -e "${TARGET_DIRNAME}" ]; then
+  cat <<HERE
+ERROR: ${TARGET_DIRNAME} directory already exists.
+
+It seems that you have already deployed this demo application.
+If you really need to re-deploy, please backup and do cleanup first!
+
+Deploy aborted. Exiting.
+HERE
+  exit 2
+fi
 
 TEMPFILE=$( mktemp -t lab5jdbc.XXXXXXXXX )
 wget ${DOWNLOAD_URL} -O $TEMPFILE
@@ -40,15 +51,29 @@ chmod 701 ${HOMEDIR}
 mkdir -p ${HOMEDIR}/public_html
 chmod 705 ${HOMEDIR}/public_html
 
-WEBDIR=$( readlink -f ./${TARGET_DIRNAME}/web )
-ln -s ${WEBDIR} ${HOMEDIR}/public_html/${WEB_DIRNAME}
+WEBDIR=$( readlink -m ./${TARGET_DIRNAME}/web )
+LINKNAME=$( readlink -m ${HOMEDIR}/public_html)/${WEB_DIRNAME}
+if [ ! -d "${WEBDIR}" ]; then
+  echo "ERROR: ${WEBDIR} not found. Deploy aborted. No cleanup done."
+  exit 3
+elif [ -e "${LINKNAME}" ] && [ ! -L "${LINKNAME}" ]; then
+  echo "ERROR: ${LINKNAME} found but is not a symbolic link. Deploy aborted. No cleanup done."
+  exit 3
+elif [ ! -L "${LINKNAME}" ]; then
+  ln -s ${WEBDIR} ${LINKNAME}
+  L_RETVAL=$?
+  if [ "${L_RETVAL}x" != "0x" ]; then
+    echo "WARNING: something strange happened when creating ${LINKNAME} See the lines above."
+  fi
+fi
 
 cat <<HERE
 Working environment was successfully set!
 
 Do not forget to:
+* review possible warning messages above.
+* change dir to workdir (cd ${TARGET_DIRNAME})
 * ensure that Oracle JDBC driver is in the lib/ subdirectory.
 * create and edit conf/preproc.config.sh (by copying preproc.config.sh.sample)
-* change dir to workdir (cd ${TARGET_DIRNAME})
 * run ./util/preproc.sh
 HERE
